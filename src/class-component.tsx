fdfdccab681a4ff7baa404c978580683b7e609ce
Samarth Porwal <samarthkporwal@gmail.com>
2023-09-26 22:19:34 +0530

    class component state included

--- src/class-component.tsx
+++ src/class-component.tsx
@@ -1,8 +1,10 @@
 import React from "react"
-
 class ClassComponentDemo extends React.Component{
+
+    state = {name:"Nitya", age:"21"};
+
     render() {
-        return <h1 className="background">This is a class Component</h1>
+        return <h1 className="background">This is a class Component {this.state.name} {this.state.age}</h1>
     }
 }
 
